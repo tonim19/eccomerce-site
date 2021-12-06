45c84bc4a545e53bf155f310d017e3e96d36b6a4
tonim19 <toni.morovic@gmail.com>
2021-12-06 16:12:01 +0100

    Getting shop data from firestore

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -5,6 +5,7 @@ import { addItem } from "../../redux/cart/cart-actions";
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,17 +2,51 @@ import { useEffect } from "react";
 import { Outlet, Route, Routes, useParams } from "react-router";
 import CollectionsOverview from "../../components/collections-overview/collections-overview";
 import CollectionPage from "../collection/collection";
+import {
+  convertCollectionsSnapshotToMap,
+  db,
+} from "../../firebase/firebase.utils";
+import { collection, getDocs } from "@firebase/firestore";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shop-actions";
+import { useState } from "react";
+import WithSpinner from "../../components/with-spinner/with-spinner";
 
-const ShopPage = () => {
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+const ShopPage = (props) => {
   let params = useParams();
-  useEffect(() => {});
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { updateCollections } = props;
+      const collectionRef = collection(db, "collections");
+      const docSnap = await getDocs(collectionRef);
+      const collectionsMap = convertCollectionsSnapshotToMap(docSnap);
+      updateCollections(collectionsMap);
+      setLoading(false);
+    };
+    fetchData();
+  });
   return (
     <div className="shop-page">
       <Routes>
-        <Route path="/" element={<CollectionsOverview />} />
+        <Route
+          path="/"
+          element={
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          }
+        />
         <Route
           path="/:collectionId"
-          element={<CollectionPage params={params} />}
+          element={
+            <CollectionPageWithSpinner
+              params={params}
+              isLoading={loading}
+              {...props}
+            />
+          }
         />
       </Routes>
       <Outlet />
@@ -20,4 +54,11 @@ const ShopPage = () => {
   );
 };
 
-export default ShopPage;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateCollections: (collectionsMap) =>
+      dispatch(updateCollections(collectionsMap)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(ShopPage);
